464fabbc0f745c64ba4802dbb1f439137075041a
Avinash Biradar <biradarav.100@gmail.com>
2022-11-22 18:12:39 +0530

    Added files

---
+++ miniProject.py
@@ -0,0 +1,22 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+sns.set_style('white')
+
+# Split
+from sklearn.model_selection import train_test_split
+
+from surprise import Reader, Dataset, SVD
+from surprise.model_selection import cross_validate
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+df = pd.read_csv("/kaggle/input/amazon-product-reviews/ratings_Electronics (1).csv",names=['userId', 'productId','rating','timestamp'])
+
+st.write(pd.DataFrame(df))
