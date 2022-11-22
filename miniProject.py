a34b4666d352eb9bcd0f992e3ca9566f71d16ab1
Avinash Biradar <biradarav.100@gmail.com>
2022-11-22 18:21:44 +0530

    Added files

--- miniProject.py
+++ miniProject.py
@@ -1,7 +1,7 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 sns.set_style('white')
