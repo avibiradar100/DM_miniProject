fa0e2599b41fb213e0c10eb904bc3032796a84fd
Avinash Biradar <biradarav.100@gmail.com>
2022-11-22 19:51:22 +0530

    Added files

--- miniProject.py
+++ miniProject.py
@@ -19,6 +19,8 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 
 df = pd.read_csv('Electronics_data.csv')
 
+st.header("Data Analysis of Amazon Reviews")
+
 st.write(pd.DataFrame(df.head(20)))
 
 st.write("Total Reviews:",df.shape[0])
